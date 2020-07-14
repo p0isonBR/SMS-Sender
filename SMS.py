3027ec7eb31a16f7c0a7f50cf1a1536d0b68824c
p0isonBR <65384918+p0isonBR@users.noreply.github.com>
2020-07-13 21:08:47 -0300

    Update SMS.py

--- SMS.py
+++ SMS.py
@@ -9,11 +9,12 @@ print("|_| by p0isonBR")
 print("    t.me/p0isonBR")
 print(" ")
 
+import requests
 import urllib.parse
 
 USER = input("Your API username: ")
 PASS = input("Your API password: ")
-NUMBER = input("Destination number, with country code: ") 
+NUM = input("Destination number, with country code: ") 
 SENDER = input("Custom sender number: ")
 TEXT = input("SMS content: ")
 
@@ -31,7 +32,7 @@ querystring = {
 "dlr-url":"https://4ba60af1.ngrok.io/receive",
 "dlr":"yes",
 "dlr-level":"3",
-"to":"NUMBER"
+"to":"NUM"
 }
 headers = {
 'cache-control': "no-cache"
